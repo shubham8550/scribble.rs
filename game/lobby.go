6ea7009f28f9b53e644ceae1818ee4c644d00b71
Shubham Badgujar <35478603+shubham8550@users.noreply.github.com>
2020-07-29 16:51:28 +0530

    Update lobby.go

--- game/lobby.go
+++ game/lobby.go
@@ -31,9 +31,9 @@ var (
 		MinRounds:            1,
 		MaxRounds:            20,
 		MinMaxPlayers:        2,
-		MaxMaxPlayers:        24,
+		MaxMaxPlayers:        150,
 		MinClientsPerIPLimit: 1,
-		MaxClientsPerIPLimit: 24,
+		MaxClientsPerIPLimit: 150,
 	}
 	SupportedLanguages = map[string]string{
 		"english": "English",
